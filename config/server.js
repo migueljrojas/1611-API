b7f73300870297bc90428910948f14475144727f
Mike <migueljrojas@gmail.com>
2020-09-28 00:05:02 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2bea30aade81ef1c150b75d0294329ab'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '464d5ba5-b88d-41fb-bfe4-7300ace40106'
+};
